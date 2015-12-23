d2905e81c6e3983b0b689cfb1875425d17a64e51
Greg Wiley <gwiley@cj.com>
2015-12-23 14:42:32 -0800

    test 5 passes

--- src/PigLatin.hs
+++ src/PigLatin.hs
@@ -1,18 +1,23 @@
 module PigLatin
-    (translate
+    (translate, unpack
     ) where
-import Data.Char (isAscii, isLetter)
+import Data.Char (isAscii, isLetter, isSpace, isPunctuation)
+import Data.List.Split (whenElt, split)
 
 translate :: String -> String
-translate = unwords . map translate1 . words
+translate = concatMap translate1 . unpack
+
+unpack :: String -> [String]
+unpack = split (whenElt (\x -> isSpace x || isPunctuation x))
 
 translate1 :: String -> String
 translate1 x
+  | null x = x
   | (not . allAsciiLetters) x = x
   | isVowel (head x)  = x ++ "yay"
   | (not . isVowel) (head x) =
       let (start,rest) = span (not . isVowel) x
        in rest ++ start ++ "ay"
   where
-    isVowel z = z `elem` "aeiouy"
+    isVowel z = z `elem` ("aeiouy" :: String)
     allAsciiLetters z = all (\l -> isAscii l && isLetter l) z
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,7 +3,7 @@ import PigLatin
 main :: IO ()
 main = do
   putStr "\n"
-  mapM_ (\x -> print x) [test1, test2, test3, test4]
+  mapM_ (\x -> print x) [test1, test2, test3, test4, test5]
 
 test1 :: Bool
 test1 = translate "the quick brown fox jumps over th3" == "ethay uickqay ownbray oxfay umpsjay overyay th3"
@@ -14,4 +14,4 @@ test3 = translate "thought x pig" == "oughtthay xay igpay"
 
 test4 = translate "at over" == "atyay overyay"
 
-test5 = translate ""
+test5 = translate "one, two, three.\nnext  line." == "oneyay, otway, eethray.\nextnay  inelay."
