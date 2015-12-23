ea822cff5a8a3af6d02c1f361d40548566553f15
Greg Wiley <gwiley@cj.com>
2015-12-23 14:16:32 -0800

    tests 1-4 pass

--- src/PigLatin.hs
+++ src/PigLatin.hs
@@ -1,5 +1,18 @@
 module PigLatin
     (translate
     ) where
+import Data.Char (isAscii, isLetter)
 
-translate = id
+translate :: String -> String
+translate = unwords . map translate1 . words
+
+translate1 :: String -> String
+translate1 x
+  | (not . allAsciiLetters) x = x
+  | isVowel (head x)  = x ++ "yay"
+  | (not . isVowel) (head x) =
+      let (start,rest) = span (not . isVowel) x
+       in rest ++ start ++ "ay"
+  where
+    isVowel z = z `elem` "aeiouy"
+    allAsciiLetters z = all (\l -> isAscii l && isLetter l) z
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,15 @@
 import PigLatin
 
 main :: IO ()
-main = mapM_ (\x -> print x) [test1, test2]
+main = do
+  putStr "\n"
+  mapM_ (\x -> print x) [test1, test2, test3, test4]
 
 test1 :: Bool
-test1 = translate "the quick brown fox jumps over" == "hetay uickqay rownay oxfay umpsjay overyay"
+test1 = translate "the quick brown fox jumps over" == "ethay uickqay ownbray oxfay umpsjay overyay"
 
 test2 = translate "one1" == "one1"
+
+test3 = translate "thought x pig" == "oughtthay xay igpay"
+
+test4 = translate "at over" == "atyay overyay"
